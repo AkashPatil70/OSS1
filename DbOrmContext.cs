9f342b3ae9cf8a24a5c48cd4a3695ec814539e88
pratimabadhekar <108924297+pratimabadhekar@users.noreply.github.com>
2022-07-08 16:21:46 +0530

    add new File

---
+++ DbOrmContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using MySql.Data.EntityFramework;
+using Day_11.Models;
+
+namespace Day_11.DAL
+{
+    [DbConfigurationType (typeof(MySqlEFConfiguration))]
+    public class DbOrmContext : DbContext //derived from DbContext
+    {
+       public DbSet<Product> products { get; set; }
+        public DbOrmContext() :base("WebAppCon")
+        {
+
+        }
+
+    }
+}
