c112db869ce528e0bc2eb1e93e066584d25e5a8b
Pratima Badhekar <108924297+pratimabadhekar@users.noreply.github.com>
2022-07-09 17:44:28 +0530

    add file
    file Added

---
+++ PRN 15/FlowersController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using WebAPIProject.Models;
+using WebAPIProject.DAL;
+
+namespace WebAPIProject.Controllers
+{
+    public class FlowersController : ApiController
+    {
+        DbORMContext entities = new DbORMContext();
+        // GET: api/Flowers
+        public IEnumerable<Product> Get()
+        {
+            List<Product> products = entities.Products.ToList();
+            return products;
+        }
+
+        // GET: api/Flowers/5
+        public Product Get(int id)
+        {
+            var product = entities.Products.SingleOrDefault(p => p.Id == id);
+            return product;
+        }
+
+        // POST: api/Flowers
+        public void Post([FromBody]Product product)
+        {
+            entities.Products.Add(product);
+
+            entities.SaveChanges();
+        }
+    
+
+        // PUT: api/Flowers/5
+        public void Put(int id, [FromBody] Product  product)
+        {
+            entities.Entry(product).State = System.Data.Entity.EntityState.Modified;
+            entities.SaveChanges();
+        }
+
+        // DELETE: api/Flowers/5
+        public void Delete(int id)
+        {
+            var product = entities.Products.SingleOrDefault(p => p.Id == id);
+            entities.Products.Remove(product ?? throw new InvalidOperationException());
+            entities.SaveChanges();   // to reflect changes back to database by Entity framework
+
+        }
+    }
+}
---
+++ PRN 15/ValuesController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPIProject.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "PGDAC", "PGDBDA", "DIOT", "DITTS","FullStack" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            string result=string.Empty;
+            switch (id)
+            {
+                case 1:
+                    result = "PGDAC";
+                    break;
+                case 2:
+                    result = "PGDBDA";
+                    break;
+                case 3:
+                    result = "DIOT";
+                    break;
+                case 4:
+                    result = "DITS";
+                    break;
+                case 5:
+                    result = "FullStack";
+                    break;
+
+            }
+            return result;
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+            string dataReceived = value;
+
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+            int idWhoseDataTobeUpdated = id;
+            string datatobeUpdated = value;
+
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+            int idWhoseDataTobeDeleted = id;
+        }
+    }
+}
